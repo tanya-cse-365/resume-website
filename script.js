b681c19f08f273e90fe3c768308a62e822a517c0
tanya-cse-365 <tanyar26725@gmail.com>
2025-12-09 12:38:14 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $(".container").animate({
+        fontSize: "18px",
+        fontWeight: "bold",
+        BackgroundColor: "#0037ffff",
+        opacity: 1}, 600);
+});
